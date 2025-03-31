5dde2df9e5ae420562c435aa3c27e06a2140698f
alexandre pavlovic-duval <alexandre.pavlovic.duval@gmail.com>
2025-03-31 23:35:29 +0200

    Creation of the HTML buttons structure and style the operators buttons part

---
+++ script.js
@@ -0,0 +1,29 @@
+// Initialization of the add and subtract with the classical functions
+function add(numA, numB) {
+    return numA + numB;
+};
+
+function subtract(numA, numB){
+    return numA - numB;
+};
+
+
+// Initialisation of the multiply and divide by arrow functionsc
+let multiply = (numA, numB) => {
+    return numA * numB;
+};
+
+let divide = (numA, numB) => {
+    return numA / numB;
+};
+
+// Initialization of the values
+let numA = 6;
+let numB = 3;
+let operator = "";
+
+// console.log("addition: ", add(numA,  numB));
+// console.log("soustraction: ", subtract(numA, numB));
+// console.log("multiplication: ", multiply(numA, numB));
+// console.log("division: ", divide(numA, numB));
+
