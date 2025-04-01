1f6e873c0d21f7282c01262a9b9497a370e2da2e
alexandre pavlovic-duval <alexandre.pavlovic.duval@gmail.com>
2025-04-01 22:17:54 +0200

    Activation of all buttons

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
+// Initialization of variables
+let operator = "";
+let numberClicked = "";
+let operatorClicked = false;
+
+// Activation of the numbers' buttons
+function getNumbers() {
+    for (let i = 0; i < document.querySelectorAll(".number").length; i++) {
+        let numberElement = document.querySelectorAll(".number")[i];
+    
+        numberElement.addEventListener("click", function () {
+            numberClicked += this.textContent;
+            document.querySelector(".screen").textContent = numberClicked;
+        })
+    }; 
+};
+
+// Activation of the operator's buttons
+function getOperators() {
+    for (let i = 0; i < document.querySelectorAll(".operator").length; i++){
+        document.querySelectorAll(".operator")[i].addEventListener("click", function () {
+            operator = this.textContent;
+            document.querySelector(".screen").textContent = operator;
+            operatorClicked = true;
+        });
+    };
+};
+
+// Activation +/-
+function negativePositiveNumber() {
+    document.querySelector(".negative-number").addEventListener("click", function () {
+    numberClicked = parseInt(numberClicked);
+    numberClicked = numberClicked * -1;
+    document.querySelector(".screen").textContent = numberClicked;
+});
+};
+
+// Activation Clear
+document.querySelector(".clear").addEventListener("click", function () {
+    numberClicked = "";
+    document.querySelector(".screen").textContent = "";
+});
+
+getNumbers();
+getOperators();
+negativePositiveNumber();
+
 // Initialization of the add and subtract with the classical functions
 function add(numA, numB) {
-    return numA + numB;
+    let score = numA + numB;
+    document.querySelector(".screen").textContent = score.toString();
 };
 
 function subtract(numA, numB){
@@ -17,13 +65,27 @@ let divide = (numA, numB) => {
     return numA / numB;
 };
 
-// Initialization of the values
-let numA = 6;
-let numB = 3;
-let operator = "";
+if (operatorClicked){
+    if (document.querySelector(".screen").textContent == "+") {
+        add(parseInt(numberClicked), parseInt(getNumbers()));
+        operatorClicked = false;
+    } else if (document.querySelector(".screen").textContent == "-") {
+        console.log("-");
+        operatorClicked = false;
+    } else if (document.querySelector(".screen").textContent == "*") {
+        console.log("*");
+        operatorClicked = false;
+    } else if (document.querySelector(".screen").textContent == "/") {
+        console.log("/");
+        operatorClicked = false;
+    };
+};
 
-// console.log("addition: ", add(numA,  numB));
-// console.log("soustraction: ", subtract(numA, numB));
-// console.log("multiplication: ", multiply(numA, numB));
-// console.log("division: ", divide(numA, numB));
-
+// let currentNumber = "";
+// for (let i = 0; i < document.querySelectorAll(".number").length; i++) {
+//     const numberElem = document.querySelectorAll(".number")[i];
+//     numberElem.addEventListener("click", function() {
+//         currentNumber += this.textContent;
+//         document.querySelector('#result').textContent = currentNumber;
+//     })
+// };
