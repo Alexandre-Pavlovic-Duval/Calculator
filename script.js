161d96a410b4197a4f349b6fb6f22321ff30741b
alexandre pavlovic-duval <alexandre.pavlovic.duval@gmail.com>
2025-04-01 23:37:44 +0200

    Add the reset function after result, and create conditions to put number into the B number to operate

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 // Initialization of variables
 let operator = "";
 let numberClicked = "";
+let numberClickedB = "";
 let operatorClicked = false;
+let resultDisplayed = false;
 
 // Activation of the numbers' buttons
 function getNumbers() {
     for (let i = 0; i < document.querySelectorAll(".number").length; i++) {
         let numberElement = document.querySelectorAll(".number")[i];
-    
+
+        // Reset after the result
         numberElement.addEventListener("click", function () {
-            numberClicked += this.textContent;
-            document.querySelector(".screen").textContent = numberClicked;
-        })
-    }; 
-};
+            if (resultDisplayed) {
+                numberClicked = "";
+                numberClickedB = "";
+                operator = "";
+                operatorClicked = false;
+                resultDisplayed = false;
+                document.querySelector(".screen").textContent = "";
+            }
+
+            if (!operatorClicked) {
+                numberClicked += this.textContent;
+                document.querySelector(".screen").textContent = numberClicked;
+            } else {
+                numberClickedB += this.textContent;
+                document.querySelector(".screen").textContent = numberClickedB;
+            }
+        });
+    }
+}
+
 
 // Activation of the operator's buttons
 function getOperators() {
@@ -38,7 +56,10 @@ function negativePositiveNumber() {
 // Activation Clear
 document.querySelector(".clear").addEventListener("click", function () {
     numberClicked = "";
-    document.querySelector(".screen").textContent = "";
+    numberClickedB = "";
+    operator = "";
+    operatorClicked = false;
+    document.querySelector(".screen").textContent = "0";
 });
 
 getNumbers();
@@ -48,44 +69,47 @@ negativePositiveNumber();
 // Initialization of the add and subtract with the classical functions
 function add(numA, numB) {
     let score = numA + numB;
-    document.querySelector(".screen").textContent = score.toString();
+    document.querySelector(".screen").textContent = score;
+    resultDisplayed = true;
 };
 
 function subtract(numA, numB){
-    return numA - numB;
+    let score = numA - numB;
+    document.querySelector(".screen").textContent = score;
+    resultDisplayed = true;
 };
 
 
 // Initialisation of the multiply and divide by arrow functionsc
 let multiply = (numA, numB) => {
-    return numA * numB;
+    let score = numA * numB;
+    document.querySelector(".screen").textContent = score;
+    resultDisplayed = true;
 };
 
 let divide = (numA, numB) => {
-    return numA / numB;
+    if (numB === 0) {
+        document.querySelector(".screen").textContent = "Error division not possible";
+    } else {
+        let score = numA / numB;
+        document.querySelector(".screen").textContent = score;
+        resultDisplayed = true;
+    }
 };
 
-if (operatorClicked){
-    if (document.querySelector(".screen").textContent == "+") {
-        add(parseInt(numberClicked), parseInt(getNumbers()));
-        operatorClicked = false;
-    } else if (document.querySelector(".screen").textContent == "-") {
-        console.log("-");
-        operatorClicked = false;
-    } else if (document.querySelector(".screen").textContent == "*") {
-        console.log("*");
-        operatorClicked = false;
-    } else if (document.querySelector(".screen").textContent == "/") {
-        console.log("/");
-        operatorClicked = false;
+// Calling of calculation function after pressing "="
+document.querySelector(".equal").addEventListener("click", function () {
+
+    if (operator === "+") {
+        add(parseInt(numberClicked), parseInt(numberClickedB));
+
+    } else if (operator === "-") {
+        subtract(parseInt(numberClicked), parseInt(numberClickedB));
+
+    } else if (operator === "*") {
+        multiply(parseInt(numberClicked), parseInt(numberClickedB));
+
+    } else if (operator === "/") {
+        divide(parseInt(numberClicked), parseInt(numberClickedB));
     };
-};
-
-// let currentNumber = "";
-// for (let i = 0; i < document.querySelectorAll(".number").length; i++) {
-//     const numberElem = document.querySelectorAll(".number")[i];
-//     numberElem.addEventListener("click", function() {
-//         currentNumber += this.textContent;
-//         document.querySelector('#result').textContent = currentNumber;
-//     })
-// };
+});
